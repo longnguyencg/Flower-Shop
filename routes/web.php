41820601ddba00d28eaa32bb90f8a8c6874ea330
buihoang <hoangbuivan0962654936@gmail.com>
2020-02-18 11:37:39 +0700

    add .

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('test', function () {
+    return view('admin.dashboard');
+
+});
