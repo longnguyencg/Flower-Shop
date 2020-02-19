2c0dcfa14ef8297112ada1d9b2e23d1429644752
ngovietan285 <ngovietan285@gmail.com>
2020-02-19 15:04:18 +0700

    sua returnShop

--- app/Http/Controllers/ReturnShopController.php
+++ app/Http/Controllers/ReturnShopController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\Color\ColorService;
+use App\Http\Services\Form\FormService;
+use App\Http\Services\Post\PostService;
+use App\Http\Services\Product\ProductService;
+use App\Http\Services\Size\SizeService;
+use App\Http\Services\Theme\ThemeService;
+use App\Http\Services\Type\TypeService;
+use App\Product;
 use Illuminate\Http\Request;
 
 class ReturnShopController extends Controller
 {
+    protected $productService;
+    protected $formService;
+    protected $typeService;
+    protected $colorService;
+    protected $sizeService;
+    protected $postService;
+    protected $themeService;
+    public function __construct(ProductService $productService, FormService $formService, TypeService $typeService, ColorService $colorService, SizeService $sizeService, PostService $postService, ThemeService $themeService )
+    {
+        $this->productService = $productService;
+        $this->formService = $formService;
+        $this->typeService = $typeService;
+        $this->colorService = $colorService;
+        $this->sizeService = $sizeService;
+        $this->postService = $postService;
+        $this->themeService = $themeService;
+    }
+
     public function index()
     {
         return view('layouts.shop.index');
